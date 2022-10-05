d5bad8506c8591a8be4fc5331aba5a0f619e7c29
Huttino <107991916+Huttino@users.noreply.github.com>
2022-10-05 09:49:15 +0200

    modified download File

--- src/main/java/ClassPrj/app/Service/Impl/DocumentServiceImpl.java
+++ src/main/java/ClassPrj/app/Service/Impl/DocumentServiceImpl.java
@@ -22,61 +22,61 @@ import java.util.List;
 @Service
 public class DocumentServiceImpl implements DocumentService {
 
-	private final DocumentRepository documentRepository;
-	private final TeacherRepository teacherRepository;
-	private final ClassRoomRepository classRoomRepository;
-	private final StudentRepository studentRepository;
-	
-	@Autowired
-	public DocumentServiceImpl(DocumentRepository documentRepository, TeacherRepository teacherRepository,
-			ClassRoomRepository classRoomRepository,StudentRepository studentRepository) {
-		super();
-		this.documentRepository = documentRepository;
-		this.teacherRepository = teacherRepository;
-		this.classRoomRepository = classRoomRepository;
-		this.studentRepository=studentRepository;
-	}
+    private final DocumentRepository documentRepository;
+    private final TeacherRepository teacherRepository;
+    private final ClassRoomRepository classRoomRepository;
+    private final StudentRepository studentRepository;
 
-	@Override
-	public List<DocumentDTO> upload(UploadDocumentWithData toUpload,Long classId,String username) {
-		List<Document> toSave=DocumentMapper.RequestToEntity(toUpload);
-		Teacher uploader=this.teacherRepository.findByUsername(username).get();
-		ClassRoom uploadedTo=this.classRoomRepository.findById(classId).get();
-		toSave.forEach(x->{
-			x.setUploadedBy(uploader);
-			x.setUploadedTo(uploadedTo);
-		});
-		List<DocumentDTO> savedDocuments=new ArrayList<>();
-		toSave.forEach(x->{
-			savedDocuments.add(DocumentMapper.entityToDTO( this.documentRepository.save(x)));
-		});
-		return savedDocuments;	
-	}
+    @Autowired
+    public DocumentServiceImpl(DocumentRepository documentRepository, TeacherRepository teacherRepository,
+                               ClassRoomRepository classRoomRepository,StudentRepository studentRepository) {
+        super();
+        this.documentRepository = documentRepository;
+        this.teacherRepository = teacherRepository;
+        this.classRoomRepository = classRoomRepository;
+        this.studentRepository=studentRepository;
+    }
 
-	@Override
-	public void delete(Long documentId,String username) {
-		Document toBeDeleted= this.documentRepository.findById(documentId).get();
-		if (toBeDeleted.getUploadedBy().getUsername().equals(username)) {
-			this.documentRepository.delete(toBeDeleted);
-		}
-		else {
-			throw new ApiException("Can't delete someone else's files");
-		}
-	}
+    @Override
+    public List<DocumentDTO> upload(UploadDocumentWithData toUpload,Long classId,String username) {
+        List<Document> toSave=DocumentMapper.RequestToEntity(toUpload);
+        Teacher uploader=this.teacherRepository.findByUsername(username).get();
+        ClassRoom uploadedTo=this.classRoomRepository.findById(classId).get();
+        toSave.forEach(x->{
+            x.setUploadedBy(uploader);
+            x.setUploadedTo(uploadedTo);
+        });
+        List<DocumentDTO> savedDocuments=new ArrayList<>();
+        toSave.forEach(x->{
+            savedDocuments.add(DocumentMapper.entityToDTO( this.documentRepository.save(x)));
+        });
+        return savedDocuments;
+    }
 
-	@Override
-	public byte[] getFile(Long id,Long userid) {
-		Document toReturn =this.documentRepository.findById(id).orElseThrow(()->{
-			throw new ApiException("document not Found");
-		});
-		if(!toReturn.getUploadedBy().getId().equals(userid)){
-			if(toReturn.getUploadedTo().getMembers().stream().noneMatch(x->{
-				return x.getId().equals(userid);
-			})){
-				throw new ApiException("You don't have access to this document");
-			}
-		}
-		return toReturn.getFile();
-	}
-	
+    @Override
+    public void delete(Long documentId,String username) {
+        Document toBeDeleted= this.documentRepository.findById(documentId).get();
+        if (toBeDeleted.getUploadedBy().getUsername().equals(username)) {
+            this.documentRepository.delete(toBeDeleted);
+        }
+        else {
+            throw new ApiException("Can't delete someone else's files");
+        }
+    }
+
+    @Override
+    public byte[] getFile(Long id,Long userid) {
+        Document toReturn =this.documentRepository.findById(id).orElseThrow(()->{
+            throw new ApiException("document not Found");
+        });
+        if(!toReturn.getUploadedBy().getId().equals(userid)){
+            if(toReturn.getUploadedTo().getMembers().stream().noneMatch(x->{
+                return x.getId().equals(userid);
+            })){
+                throw new ApiException("You don't have access to this document");
+            }
+        }
+        return toReturn.getFile();
+    }
+
 }
